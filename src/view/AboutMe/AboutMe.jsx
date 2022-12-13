9e339fe8fb52f06ac9a68fcee3340c3807ae73a3
Soocyang <36953143+Soocyang@users.noreply.github.com>
2022-12-13 17:12:13 +0800

    refactor about content

--- src/view/AboutMe/AboutMe.jsx
+++ src/view/AboutMe/AboutMe.jsx
@@ -7,6 +7,8 @@ import VSCodeLogo from '../../assets/svg/vscode.svg';
 import ProfilePic from '../../assets/img/Standard_Profile_Pic_square.jpg';
 import './aboutme.css';
 
+import aboutContent from '../../db/about-me.json'
+
 // Importing react swiper lib
 // Direct React component imports
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -16,23 +18,15 @@ import "swiper/css";
 import "swiper/css/effect-cards";
 
 const AboutMe = () => {
+	const [about, techStack] = aboutContent
+
 	return (
 		<section id='about-me' className='about-me'>
 			<div className='content'>
 				<div className="container-flex">
 					<div className='about-me-description'>
-						<h1>About me</h1>
-						<p>
-							Passionate about creating and building websites.
-							I also enjoy learning new technology and tools that can help me achieve my
-							goals. In most of the time I'm more interested in front-end development.
-							Currently working as a full-stack developer in
-							<a href="https://www.fusionexgroup.com"> Fusionex Group</a> after graduating from
-							<a href="https://www.tarc.edu.my"> Tunku Abdul Rahman University College </a>
-							I'm learning on how to creates a website that starts from
-							wireframing UI/UX to the coding of the web pages. Feel free to browse
-							through my old projects and works during my studies.
-						</p>
+						<h1>{about.title}</h1>
+						<p>{about.content}</p>
 					</div>
 					<div className="img-me">
 						<img src={ProfilePic} alt='temp img' width={300} height={300} />
@@ -40,14 +34,8 @@ const AboutMe = () => {
 				</div>
 				<div className="container-flex">
 					<div className='tech-stack-description'>
-						<h1>Tech Stack</h1>
-						<p>
-							During my CS Degree in Sofware Development. I have learned many technology such as
-							Java, ASP.NET Core, MySQL and MSSQL, Html, Css, and Javascript and etc in order to
-							complete my studies. Then, during my internship I have learned ReactJs for front-end and
-							Java Springboot as back-end. Now, I'm learning Vue and doing NodeJs application as the my
-							working environment requires.
-						</p>
+						<h1>{techStack.title}</h1>
+						<p>{techStack.content}</p>
 					</div>
 					<Swiper
 						effect={"cards"}

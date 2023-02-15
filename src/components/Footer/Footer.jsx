9eb4e57f6d811caaed18798d7dd90c5527760111
Soocyang <36953143+Soocyang@users.noreply.github.com>
2023-02-15 23:30:36 +0800

    test api

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,22 @@
 import './footer.css'
 import LogoSvg from '../../assets/svg/logo.sun.og.svg'
 
+const handleTest = async () => {
+  const response = await fetch('https://blog-app-api-production-4eb3.up.railway.app/test')
+  console.log(await response.text())
+}
+
 const Footer = () => {
   return (
     <section id='footer' className='footer'>
-        <div className="container-flex">
-          <span>
-            <img className="footer-icon" src={LogoSvg} alt='logo' width={50} />
-            <span>Lim Soo Cia Yang © 2022</span>
-          </span>
-      </div></section>
+      <div className="container-flex">
+        <span>
+          <img className="footer-icon" src={LogoSvg} alt='logo' width={50} />
+          <span>Lim Soo Cia Yang © 2022</span>
+        </span>
+      </div>
+      <button onClick={handleTest}>test</button>
+    </section>
   )
 }
 
